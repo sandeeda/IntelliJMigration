92c169e2613a11493fe9d68cf7cf0d47409cab5c
Sandeep Das <sdsts28@outlook.com>
2023-02-20 16:21:17 -0500

    first commit

---
+++ src/main/java/learn/intellij/streams2/Streams2Application.java
@@ -0,0 +1,13 @@
+package learn.intellij.streams2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Streams2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Streams2Application.class, args);
+	}
+
+}
---
+++ src/test/java/learn/intellij/streams2/Streams2ApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.intellij.streams2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Streams2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
